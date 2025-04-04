cd9292f6a88cf2a6fa322bb40bb6d311fd9325c7
davviie <davidtrudelbray@proton.me>
2025-04-04 20:33:03 +0000

    sync

--- start.sh
+++ start.sh
@@ -135,7 +135,7 @@ services:
         mkdir -p /etc/apt/keyrings &&
         curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc &&
         chmod a+r /etc/apt/keyrings/docker.asc &&
-        echo 'deb [arch=\$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu stable' > /etc/apt/sources.list.d/docker.list &&
+        echo 'deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu stable' > /etc/apt/sources.list.d/docker.list &&
         apt-get update &&
         apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin;
       else
