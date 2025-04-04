f50f95603094bde337007376e891e41022f39d0b
davviie <davidtrudelbray@proton.me>
2025-04-04 20:03:37 +0000

    1

--- start.sh
+++ start.sh
@@ -182,7 +182,8 @@ services:
       mkdir -p /etc/apt/keyrings &&
       curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc &&
       chmod a+r /etc/apt/keyrings/docker.asc &&
-      echo 'deb [arch=\$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \$(. /etc/os-release && echo \${UBUNTU_CODENAME:-\$VERSION_CODENAME}) stable' > /etc/apt/sources.list.d/docker.list &&
+      . /etc/os-release &&
+      echo 'deb [arch=\$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \${UBUNTU_CODENAME:-\$VERSION_CODENAME} stable' > /etc/apt/sources.list.d/docker.list &&
       apt-get update &&
       apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin &&
       dockerd --debug --host=tcp://0.0.0.0:2375 --host=unix:///var/run/docker.sock --storage-driver=$STORAGE_DRIVER --tls=false
