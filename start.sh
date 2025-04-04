0e616de0a64f05d8736d4eb86fff8ef8423b7129
davviie <davidtrudelbray@proton.me>
2025-04-04 20:31:21 +0000

    sync

--- start.sh
+++ start.sh
@@ -126,8 +126,8 @@ services:
       sh -c "
       if [ -f /etc/alpine-release ]; then
         echo 'Detected Alpine Linux. Using apk for package installation...' &&
-        apk add --no-cache curl ca-certificates gnupg &&
-        apk add --no-cache docker-cli docker-compose-plugin;
+        apk add --no-cache curl ca-certificates &&
+        apk add --no-cache docker-cli docker-compose;
       elif [ -f /etc/os-release ] && grep -qi 'ubuntu\|debian' /etc/os-release; then
         echo 'Detected Ubuntu/Debian. Using apt-get for package installation...' &&
         apt-get update &&
@@ -135,7 +135,7 @@ services:
         mkdir -p /etc/apt/keyrings &&
         curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc &&
         chmod a+r /etc/apt/keyrings/docker.asc &&
-        echo 'deb [arch=\$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \$(. /etc/os-release && echo \${UBUNTU_CODENAME:-\$VERSION_CODENAME}) stable' > /etc/apt/sources.list.d/docker.list &&
+        echo 'deb [arch=\$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu stable' > /etc/apt/sources.list.d/docker.list &&
         apt-get update &&
         apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin;
       else
