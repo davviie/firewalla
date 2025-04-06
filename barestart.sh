3a958c684da2555d903a2d22a9948a7f5c50fe9a
davviie <davidtrudelbray@proton.me>
2025-04-06 03:39:57 +0000

    bind to ./

--- barestart.sh
+++ barestart.sh
@@ -1,2 +1,11 @@
 systemctl start docker
 systemctl enable docker
+sudo usermod -aG docker pi
+## how to refresh here without logout??##
+
+#enter the docker-in-docker container 
+docker exec -it docker-in-docker sh
+##install compose inside the dind container
+apk add --no-cache docker-compose
+
+
